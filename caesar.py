f508b61eebd6b630ed777f3935b678626a4fa1df
Oisin Whelan <oisin.whelan@ucdconnect.ie>
2021-05-02 16:54:40 +0100

    Error cases added

--- caesar.py
+++ caesar.py
@@ -1,4 +1,11 @@
 state = input("Would you like to (g)enerate or (s)olve?\n")
-string = input("Enter your string:\n")
+while True:
+    string = input("Enter your string:\n")
+    if not string:
+        print("Please enter a string.\n")
+    elif string.isalpha():
+        break
+    else:
+        print("Please only enter English alphabet letters.\n")
 num = input("Enter the cipher's magnitude:\n")
 
